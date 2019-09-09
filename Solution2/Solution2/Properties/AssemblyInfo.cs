40d3db032402eb840dba9f613d603eb5b52e186d
Sss41 <silvana.souza63@gmail.com>
2019-09-09 15:52:07 -0300

    Segundo Commit

--- Solution2/Solution2/Properties/AssemblyInfo.cs
+++ Solution2/Solution2/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // As informações gerais sobre um assembly são controladas por
 // conjunto de atributos. Altere estes valores de atributo para modificar as informações
 // associadas a um assembly.
-[assembly: AssemblyTitle("ConsoleApp1")]
+[assembly: AssemblyTitle("Solution2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp1")]
+[assembly: AssemblyProduct("Solution2")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
-[assembly: Guid("cdd01b0a-8fb7-4941-856a-1ed51674917d")]
+[assembly: Guid("2717ad93-c7bf-4a1a-be86-45cc2b346f6f")]
 
 // As informações da versão de um assembly consistem nos quatro valores a seguir:
 //
