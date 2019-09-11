fe7ba00c1a10f0e299ffb0ca0b66f62e57833603
conta para visual studio <cmvs19@outlook.com>
2019-09-11 17:16:50 -0300

    Aula 11/09/19

---
+++ consoleapp1/consoleapp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consoleapp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var primeiroTexto = "Oi meu nome Farias e logo eu Farias tenho vontade de codar muito porque eu Farias gosto de codar durante a minha vida";
+            primeiroTexto = primeiroTexto.Replace("Farias", "1").Replace("codar", "2");
+
+            Console.WriteLine(primeiroTexto);
+            Console.WriteLine($"Texto contém:{primeiroTexto.Length} caracteres.");
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
