3f0f3aa4e20dc09ab912f8ffa4da4549c9c50cee
conta para visual studio <cmvs19@outlook.com>
2019-09-12 16:42:59 -0300

    Material aula 12/09

---
+++ lacosderepeticao/UtilizandoFor/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilizandoFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nome = "Farias Gabriel";
+            var countFind = 0;
+
+            Console.WriteLine("Encontre a palavra até 3 caracteres:");
+            var palavra = Console.ReadLine();
+
+            for (int i = 0; i < (nome.Length -2); i++) 
+            {
+
+                var palavraParacomparar = nome[i].ToString() +
+                                          nome[i + 1].ToString() +
+                                          nome[i + 2].ToString();
+
+                if (palavra == palavraParacomparar)
+                    countFind++;
+            }
+
+            Console.WriteLine($"quantidade total de '{palavra}' {countFind}");
+
+        Console.ReadKey();
+        }
+    }
+}
