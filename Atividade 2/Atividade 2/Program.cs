2aa96e305f87d7b6d4ca8bd3af3f957a1977470c
conta para visual studio <cmvs19@outlook.com>
2019-09-13 15:15:13 -0300

    Atividade 2 mensagem de boas vindas

---
+++ Atividade 2/Atividade 2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o seu nome:" +
+                           "");
+
+            
+
+                var nome = Console.ReadLine();
+            if (nome == "jair bolsonaro") Console.WriteLine($"Você não é bem vindo, vai tomar no c# {nome}");
+            else Console.WriteLine($"Seja bem vindo {nome} ");
+
+            Console.ReadKey();
+
+        }
+    }
+}
