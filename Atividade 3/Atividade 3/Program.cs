01d2cc664c203b69dd5ddc6b5ecaa080332075b3
conta para visual studio <cmvs19@outlook.com>
2019-09-13 15:26:52 -0300

    Atividade 3 Calma jovi

---
+++ Atividade 3/Atividade 3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Informe o seu nome:" +"");
+            var nome = Console.ReadLine ();
+
+            Console.WriteLine("Informe a sua idade:" + "");
+
+            var idade = int.Parse (Console.ReadLine());
+
+            if (idade <= 18) Console.WriteLine($"Calma jovi {nome} tudo ao seu tempo, logo você terá 18 anos de idade.");
+
+            else Console.WriteLine($"Seja bem vindo {nome}");
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
