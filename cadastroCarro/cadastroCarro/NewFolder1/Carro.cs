1e7ac326bb713581792089c01c9532135eade03c
conta para visual studio <cmvs19@outlook.com>
2019-09-19 13:38:02 -0300

    commit cadastroCarro

--- cadastroCarro/cadastroCarro/NewFolder1/Carro.cs
+++ cadastroCarro/cadastroCarro/NewFolder1/Carro.cs
@@ -4,9 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace cadastroCarro.NewFolder1
 {
     class Carro
     {
+        public string Marca { get; set; }
+
+        public string Modelo { get; set; }
+
+        public int Ano { get; set; }
+
+        public int valor { get; set; }
+
+
     }
 }
